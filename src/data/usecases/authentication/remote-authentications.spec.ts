6e05e7d12bf2c8a71334f2ef8b5e452769fc1361
Alyson Henrique <alysonhenrique26@gmail.com>
2021-12-02 11:18:48 -0300

    test: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/usecases/authentication/remote-authentications.spec.ts
+++ src/data/usecases/authentication/remote-authentications.spec.ts
@@ -4,6 +4,7 @@ import { HttpStatusCode } from "@/data/protocols/http/http-response"
 import { mockAuthentication } from "@/domain/test/mock-authentication"
 import { InvalidCredentialsError } from "@/domain/errors/InvalidCredentialsError"
 import faker from 'faker'
+import { UnexpectedError } from "@/domain/errors/UnexpectedError"
 
 type SutTypes = {
     sut: RemoteAuthentication
@@ -34,6 +35,15 @@ describe('RemoteAuthentication', () => {
         expect(httpPostClientSpy.body).toEqual(authenticationParams)
     })
 
+    test('Should throw UnexpectedError if HttpPostCllient returns 400', async () => { 
+        const { sut, httpPostClientSpy } = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.badRequest
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
     test('Should throw InvalidCredencialsError if HttpPostCllient returns 401', async () => { 
         const { sut, httpPostClientSpy } = makeSut()
         httpPostClientSpy.response = {
@@ -41,5 +51,23 @@ describe('RemoteAuthentication', () => {
         }
         const promise = sut.auth(mockAuthentication())
         await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+    })
+
+    test('Should throw UnexpectedError if HttpPostCllient returns 404', async () => { 
+        const { sut, httpPostClientSpy } = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.notFound
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
+    test('Should throw UnexpectedError if HttpPostCllient returns 500', async () => { 
+        const { sut, httpPostClientSpy } = makeSut()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.serverError
+        }
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
     })
 })
