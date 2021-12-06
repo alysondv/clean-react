1c34da3253025acb481385bd9cb62ab431095d4c
Alyson Henrique <alysonhenrique26@gmail.com>
2021-12-06 12:19:05 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extension: ['.ts', '.tsx', '.js', 'scss'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
@@ -35,9 +35,14 @@ module.exports = {
         }]
     },
     devServer: {
-        contentBase: './public',
-        writeToDisk: true,
-        historyApiFallback: true
+        devMiddleware: {
+          writeToDisk: true
+        },
+        static: {
+          directory: './public'
+        },
+        historyApiFallback: true,
+        port: 8080
     },
     externals: {
         react: 'React',
